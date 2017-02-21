9e6e9e76f8d97d2b1c75601df70159f3ae0db2b1
jonatan jorge <jonh_gabriel05@hotmail.com>
2017-02-21 17:22:25 -0300

    seeds

---
+++ database/seeds/ClientsSeedersConFaker.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ClientsSeedersConFaker extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create('es_AR');
+
+        for ($i = 1; $i < 65; $i++){
+            DB::table('clients')->insert([
+                [
+                    'id' => $i,
+                    'name' => $faker->firstName,
+                    'last_name' => $faker->lastName,
+                    'email' => $faker->freeEmail,
+                    'dni' => Faker\Provider\es_Es\Person::dni(),
+                    'sexo' => $faker->randomElement(['masculino','femenino']),
+                    'dob' => $faker->dateTimeThisCentury->format('Y-m-d'),
+                    'address' => $faker->address,
+                    'prospecto' => $faker->boolean,
+                    'phone1' => $faker->phoneNumber,
+                    'nacionality' => Faker\Provider\es_Ar\Address::country(),
+                    'city' => $faker->city,
+                    'location' => Faker\Provider\es_Ar\Address::community(),
+                    'province' => Faker\Provider\es_Ar\Address::state(),
+
+                ]
+
+            ]);
+        }
+    }
+}
