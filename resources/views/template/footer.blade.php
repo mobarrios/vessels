f3cb7dc634a0c19aac898ce3f4d64c89a0c3e183
Manuel Barrios <mbarrios@desarrollosocial.gob.ar>
2017-03-23 15:41:35 -0300

    git versions

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,7 +1,8 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        branch: <b>Motonet</b> |
-        Version: <b>0.0.1</b>
+
+        branch: <b>{{shell_exec('git branch | grep \*')}}</b> |
+        Version: <b>{{shell_exec('git describe --tags')}}</b>
     </div>
     <strong>NavCoder</strong> All rights
     reserved.
