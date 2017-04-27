86f20a376209b24e9ea5d9e768ea0d21f040d7e0
jonatan jorge <jonh_gabriel05@hotmail.com>
2017-04-27 15:41:00 -0300

    form12

---
+++ resources/views/moto/files/formulario/form12.blade.php
@@ -0,0 +1,165 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>formulario 12</title>
+
+    <style>
+
+        *{
+            padding: 0;
+            margin: 0;
+        }
+
+        #contenedor{
+            height: 29cm;
+            position: relative;
+            top:0;
+            left:0;
+        }
+
+        p{
+            position: absolute;
+        }
+
+
+        #chapa{
+            top: 4.8cm;
+            left: 8cm;
+        }
+
+        #marca{
+            top: 7.1cm;
+            left: 6.2cm;
+        }
+
+        #tipo{
+            top: 7.8cm;
+            left: 5.8cm;
+        }
+
+
+        #modelo{
+            top: 8.5cm;
+            left: 6.5cm;
+        }
+
+        #marca_motor{
+            top: 9.1cm;
+            left: 7.8cm;
+        }
+
+        #n_motor{
+            top: 9.8cm;
+            left: 7.5cm;
+        }
+
+        #marca_cuadro{
+            top: 10.5cm;
+            left: 8.5cm;
+        }
+
+        #n_cuadro{
+            top: 11.2cm;
+            left: 7.5cm;
+        }
+
+
+
+
+
+
+
+        #apellido_nombre{
+            top: 23.9cm;
+            left: 9.3cm;
+        }
+
+        #dni{
+            top: 24.5cm;
+            left: 10.3cm;
+        }
+
+        #calle{
+            top: 25.2cm;
+            left: 7.5cm;
+        }
+
+        #numero{
+            top: 25.2cm;
+            left: 13cm;
+        }
+
+        #localidad{
+            top: 25.2cm;
+            left: 16cm;
+        }
+
+
+
+    </style>
+
+</head>
+<body>
+    <div id="contenedor">
+        <p id="chapa">dsasad66551321355</p>
+
+        <p id="marca">
+            Yamaha
+        </p>
+
+        <p id="tipo">
+            Moto
+        </p>
+
+        <p id="modelo">
+            FZ150
+        </p>
+
+        <p id="marca_motor">
+            Yamaha
+        </p>
+
+        <p id="n_motor">
+            13515132132135135135
+        </p>
+
+        <p id="marca_cuadro">
+            Yamaha
+        </p>
+
+        <p id="n_cuadro">
+            151d3s13das153513
+        </p>
+
+
+
+
+        <p id="apellido_nombre">
+            Barrios Manuel
+        </p>
+
+        <p id="dni">
+            27156488
+        </p>
+
+        <p id="calle">
+            Tu calle
+        </p>
+
+        <p id="numero">
+            222
+        </p>
+
+        <p id="localidad">
+            Hurlingham
+        </p>
+
+    </div>
+
+
+</body>
+</html>
